6dd13685014c83fb0d61777b240c57cfb70f293b
habibmac <mcrury@gmail.com>
2024-04-30 17:41:09 +0700

    chore: vite & tw configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 import('tailwindcss').Config
+
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.{js,ts,vue}',
+    './**/*.php',
+    "./inc/**/*.php",
+  ],
   theme: {
     extend: {},
   },
